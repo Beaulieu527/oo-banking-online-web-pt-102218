45933d27ee5d32d712af6c0c73edb3b2aececce0
Justin Beaulieu <beaulieudev@gmail.com>
2018-12-01 19:58:53 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,24 @@
 class BankAccount
+  attr_accessor :name, :balance, :status
+
+  def initialize
+    @name = name
+    @balance = 1000
+    @status = open
+  end
+
+  def desposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+  end
+
+  def valid?
+  end
+
+  def close_account
+  end
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :reciever, :status, :amount
+
+  def initialize
+    @sender = sender
+    @reciever = reciever
+    @status = pending
+    @amount = amount
+  end
+
+  
 end
