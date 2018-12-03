83a26d9558c8a19bdbb196852d32519059d240fd
Justin Beaulieu <beaulieudev@gmail.com>
2018-12-03 02:27:59 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,26 @@
 class BankAccount
+  attr_accessor  :bank_account, :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @bank_account = bank_account
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance = @balance + amount
+  end
+
+  def display_balance
+  end
+
+  def valid?
+  end
+
+  def close_account
+  end
+
 
 end
